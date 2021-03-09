77a24e55ca8252861ca18011ea85f5f57dbfbaad
Sanjiv chaudhary <sanjivchudhary416@gmail.com>
2021-03-09 12:46:06 +0545

    contct section

--- js/main.js
+++ js/main.js
@@ -120,14 +120,7 @@
     time: 1000
   });
 
-  // Skills section
-  $('.skills-content').waypoint(function() {
-    $('.progress .progress-bar').each(function() {
-      $(this).css("width", $(this).attr("aria-valuenow") + '%');
-    });
-  }, {
-    offset: '80%'
-  });
+
 
   // Porfolio isotope and filter
   $(window).on('load', function() {
